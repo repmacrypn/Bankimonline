c56a158013ec298658d65f352f0bbb2f79e4f67d
repmacrypn <artik3267@gmail.com>
2023-09-02 18:20:24 +0300

    feet+fix: created and styled header component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../Header'
 
+import styles from './styles.module.scss'
+
 export const App = () => {
     return (
-        <>
+        <div className={styles.appWrapper}>
             <Header />
-        </>
+        </div>
     )
 }
